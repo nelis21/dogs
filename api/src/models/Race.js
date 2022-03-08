d6ae1da315292ff505155f3c249f3ed82e06cc0f
nelis21 <nellygallardo21@gmail.com>
2022-03-08 17:36:47 -0300

    .\api\

---
+++ api/src/models/Race.js
@@ -0,0 +1,43 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('race', {
+     id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    life_span: {
+      type: DataTypes.STRING
+
+    },
+
+    image: {
+      type: DataTypes.STRING,
+    },
+
+    apiId: {
+      type: DataTypes.INTEGER,
+    }
+
+
+  }, { timestamps: false });
+};
